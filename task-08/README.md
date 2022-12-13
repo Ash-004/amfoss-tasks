e68af02446e88bcd297dd773eb3bc83138330745
Ash-004 <ash_004@icloud.com>
2022-12-14 05:07:41 +0530

    InternetExplorer

---
+++ task-08/README.md
@@ -0,0 +1,4 @@
+# Task08
+## Internet Explorer 🌐
+This was a pretty straighforward task. The reference articles were more than enough even for me as I am a complete beginner at webdev.
+I did spend a few hours scratching my head over the js part which actually seemed easy once I understood how to make it work.
---
+++ task-08/index.js
@@ -0,0 +1,7 @@
+let play1 = function(){document.getElementById("audio1").play()}
+let play2 = function(){document.getElementById("audio2").play()}
+let play3 = function(){document.getElementById("audio3").play()}
+let play4 = function(){document.getElementById("audio4").play()}
+let play5 = function(){document.getElementById("audio5").play()}
+let play6 = function(){document.getElementById("audio6").play()}
+let play7 = function(){document.getElementById("audio7").play()}
